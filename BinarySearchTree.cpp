6b6d42c73fa494466d101c2a381ce5a21a2ed1d3
Armando Galván <armandocgv@gmail.com>
2016-06-07 10:24:37 -0500

    BST actualizado

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,4 +1,4 @@
-class node 
+struct node 
 {
     int data;
     node* left;
@@ -108,5 +108,16 @@ node * insert(node * root, int value)
         root->right = insert(root->right, value);
     }
 
+    return root;
+}
+
+node * lca(node * root, int v1,int v2)
+{
+    if (root->data < v1 && root->data < v2)
+        return lca(root->right, v1, v2);
+    
+    if (root->data > v1 && root->data > v2)
+        return lca(root->left, v1, v2);
+    
     return root;
 }
